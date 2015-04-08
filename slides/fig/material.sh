f5ff7e23a21e40aa6d945165ceab6ab4dc2d032d
David Deharbe <deharbe@gmail.com>
2015-04-08 15:24:39 -0300

    + aula 14, u aula 13

--- slides/fig/material.sh
+++ slides/fig/material.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+function aula13 () {
 file=stack-array
 pdflatex $file
 ./splitPDF.py $file.pdf 1 2 3 4 5
@@ -14,3 +15,59 @@ file=deque-array
 pdflatex $file
 ./splitPDF.py $file.pdf 1 2 3 4 5
 for i in 1 2 3 4 5; do mv $file.part$i.$i'_'$i.pdf "$file-$i.pdf"; done
+}
+
+function aula14 () {
+
+TEXFILES="singly-linked-list \
+  singly-linked-list-memory \
+  singly-linked-list-circular \
+  singly-linked-list-circular-memory \
+  doubly-linked-list \
+  doubly-linked-list-memory \
+  doubly-linked-list-circular \
+  doubly-linked-list-circular-memory \
+  singly-linked-list-insert-after \
+  singly-linked-list-insert-first \
+  doubly-linked-list-insert-after\
+  doubly-linked-list-sentinel-circular-remove\
+  doubly-linked-list-insert-before"
+
+for file in $TEXFILES; do
+  pdflatex $file
+done
+
+file=singly-linked-list-insert-after
+./splitPDF.py $file.pdf 1 2 3
+for i in 1 2 3; do mv $file.part$i.$i'_'$i.pdf "$file-$i.pdf"; done
+
+file=singly-linked-list-insert-first
+./splitPDF.py $file.pdf 1 2 3
+for i in 1 2 3; do mv $file.part$i.$i'_'$i.pdf "$file-$i.pdf"; done
+
+file=doubly-linked-list-insert-after
+pages="1 2 3 4 5"
+./splitPDF.py $file.pdf $pages
+for i in $pages; do mv $file.part$i.$i'_'$i.pdf "$file-$i.pdf"; done
+
+file=doubly-linked-list-insert-before
+pages="1 2 3 4 5"
+./splitPDF.py $file.pdf $pages
+for i in $pages; do mv $file.part$i.$i'_'$i.pdf "$file-$i.pdf"; done
+
+file=singly-linked-list-remove-after
+pages="1 2"
+./splitPDF.py $file.pdf $pages
+for i in $pages; do mv $file.part$i.$i'_'$i.pdf "$file-$i.pdf"; done
+
+file=singly-linked-list-remove-first
+pages="1 2"
+./splitPDF.py $file.pdf $pages
+for i in $pages; do mv $file.part$i.$i'_'$i.pdf "$file-$i.pdf"; done
+
+file=doubly-linked-list-sentinel-circular-remove
+pages="1 2 3 4"
+./splitPDF.py $file.pdf $pages
+for i in $pages; do mv $file.part$i.$i'_'$i.pdf "$file-$i.pdf"; done
+
+}
