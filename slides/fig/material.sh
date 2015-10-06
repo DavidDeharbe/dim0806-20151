1713839eaa97058a182913de4a776e23ac52349d
David Deharbe <deharbe@gmail.com>
2015-10-06 14:59:59 -0300

    update

--- slides/fig/material.sh
+++ slides/fig/material.sh
@@ -1,6 +1,53 @@
 #!/bin/bash
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#                             material.sh
+#
+# This file contains bash functions that produce figures for the
+# slides in course DIM0806.
+#
+# 1. in the command line, type:
+#   % source material.sh
+# 2. call the function corresponding to the lecture for which figures 
+# need # to be produced, e.g.
+#   % aula12
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+function aula10 () {
+file=heap-insert
+pages="1 2 3 4 5 6"
+pdflatex $file
+./splitPDF.py $file.pdf $pages
+for i in $pages; do mv $file.part$i.$i'_'$i.pdf "$file-$i.pdf"; done
+
+file=heap-2
+pages="1 2 3 4 5 6 7 8 9 10 11"
+pdflatex $file
+./splitPDF.py $file.pdf $pages
+for i in $pages; do mv $file.part$i.$i'_'$i.pdf "$file-$i.pdf"; done
+
+file=heap-build
+pages="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17"
+pdflatex $file
+./splitPDF.py $file.pdf $pages
+for i in $pages; do mv $file.part$i.$i'_'$i.pdf "$file-$i.pdf"; done
+}
+echo "aula10: heaps"
+
+function aula12 () {
+  latexmk -pdf decision-tree-sort3
+  latexmk -pdf bucket-sort
+}
+echo "aula12: ordenação, cota mínima, ordenação em tempo linear"
+
 function aula13 () {
+
+file=stack
+pdflatex $file
+./splitPDF.py $file.pdf 1 2 3 4
+for i in 1 2 3 4; do mv $file.part$i.$i'_'$i.pdf "$file-$i.pdf"; done
+
 file=stack-array
 pdflatex $file
 ./splitPDF.py $file.pdf 1 2 3 4 5
@@ -16,6 +63,8 @@ pdflatex $file
 ./splitPDF.py $file.pdf 1 2 3 4 5
 for i in 1 2 3 4 5; do mv $file.part$i.$i'_'$i.pdf "$file-$i.pdf"; done
 }
+echo "aula13: pilhas, filas, deques"
+
 function aula14 () {
 
 TEXFILES="singly-linked-list \
@@ -72,6 +121,7 @@ pages="1 2 3 4"
 for i in $pages; do mv $file.part$i.$i'_'$i.pdf "$file-$i.pdf"; done
 
 }
+echo "aula14: linked lists"
 
 function aula16 () {
 
@@ -88,6 +138,18 @@ pages="1 2"
 for i in $pages; do mv $file.part$i.$i'_'$i.pdf "$file-$i.pdf"; done
 
 }
+echo "aula16: binary trees"
+
+function aula17 () {
+
+TEXFILES="avl"
+
+for file in $TEXFILES; do
+  pdflatex $file
+done
+
+}
+echo "aula17: avl trees"
 
 function aula21 () {
 
